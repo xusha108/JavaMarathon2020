9810a52c51aa66fdf3cb9d613b54d1e62c66f145
unknown <xusha108@gmail.com>
2020-11-30 11:14:16 +0300

    solution day1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int value = 0;
+        while(value<10){
+            System.out.print("JAVA ");
+            value = value+1;
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k=6;
+        for(int i=1; i<=10; i++){
+            int sum = i*k;
+            System.out.println(i + " x " + k + " = " + sum);
+        }
     }
 }
