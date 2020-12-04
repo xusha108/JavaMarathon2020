8fa642aed7c5fbcfcbb92e30ad5d6222e677eddd
unknown <xusha108@gmail.com>
2020-12-04 10:50:16 +0300

    solution day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,53 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setYear(-15);
+        car1.setColor("Blue");
+        car1.setModel("Toyota");
+        System.out.println("Год выпуска: " + car1.getYear());
+        System.out.println("Цвет: " + car1.getColor());
+        System.out.println("Модель: " + car1.getModel());
+    }
+}
 
+class Car {
+    private int year;
+    private String color;
+    private String model;
+
+    public void setYear(int userYear) {
+        if (userYear < 0) {
+            System.out.println("Введите правильный год выпуска машины");
+        } else {
+            year = userYear;
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setColor(String userColor) {
+        if (userColor.isEmpty()) {
+            System.out.println("Заполните поле");
+        } else {
+            color = userColor;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String userModel) {
+        if (userModel.isEmpty()) {
+            System.out.println("Заполните поле");
+        } else {
+            model = userModel;
+        }
+    }
+    public String getModel() {
+        return model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,33 @@
 package day5;
 
-
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike = new Motorbike(2015, "red", "Suzuki");
+        System.out.println("Color: " + motorbike.getColor());
+        System.out.println("Model: " + motorbike.getModel());
+        System.out.println("Year: " + motorbike.getYear());
     }
 }
+
+class Motorbike {
+    private int year;
+    private String color;
+    private String model;
+
+    public Motorbike(int year, String color, String model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getModel() {
+        return model;
+    }
+}
+
