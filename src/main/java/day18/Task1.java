035d51d9aab5affc66509183a419ce92c142c1b5
unknown <xusha108@gmail.com>
2020-12-18 13:05:08 +0300

    tasks solution day18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,25 @@
+//Вам дан числовой массив произвольного размера. Посчитайте сумму чисел в этом массиве не используя циклы
+// (необходимо использовать рекурсивные вызовы).
+//
+//Пример:
+//int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+//System.out.println(recursionSum(...)); // 65346
+//
+//*recursionSum(...) - это ваш рекурсивный метод, который вернет сумму чисел в массиве numbers.
+
+
 package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(recursionSum(numbers, 0));
+    }
 
+
+    public static int recursionSum(int[] numbers, int i) {
+        if (i == (numbers.length - 1))
+            return numbers[i];
+        return numbers[i] + recursionSum(numbers, i + 1);
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,7 +1,36 @@
+//Создайте рекурсивный метод, который принимает на вход единственный аргумент - число, и возвращает количество цифр
+// 7 в этом числе. Циклы использовать запрещено, можно использовать только рекурсивные вызовы.
+//
+//Пример:
+//System.out.println(count7(717771237)); // 5
+
+
 package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(count7(717771237));
 
+    }
+
+    public static int count7(int total) {
+        String strSum = Integer.toString(total);
+        String nums[] = strSum.split("");
+       return recursionSum(nums, 0);
+
+    }
+
+    public static int recursionSum(String nums[], int i) {
+        if (i == nums.length - 1) {
+            if (Integer.parseInt(nums[i]) == 7) {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (Integer.parseInt(nums[i]) == 7) {
+            return 1 + recursionSum(nums, i + 1);
+        }
+        return 0 + recursionSum(nums, i + 1);
     }
 }
